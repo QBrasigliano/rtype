efb2a0ae4e94b743c7cae4ecd5d5903fd12abc6d
Quentin Brasigliano <quentin.brasigliano@epitech.eu>
2026-03-05 10:15:46 +0100

    sauvegarde sans l'engine et server

--- engine/include/Component.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Base class for all components
-class Component {
-public:
-    virtual ~Component() = default;
-};
--- engine/src/Entity.cpp
+++
@@ -1,5 +0,0 @@
-#include "../include/Entity.hpp"
-
-uint32_t Entity::next_id_ = 0;
-
-Entity::Entity() : id_(next_id_++) {}
